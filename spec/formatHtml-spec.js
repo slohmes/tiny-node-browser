e43b6882e1684163051062c037c6257a63366538
Sarah Lohmeier <sarahlohmes@gmail.com>
2018-04-26 13:44:40 -0400

    Refactors to use a Token class for html tokens instead of a plain js object

--- spec/formatHtml-spec.js
+++ spec/formatHtml-spec.js
@@ -5,8 +5,11 @@ describe('formatHtml', () => {
   it('should return plain text unchanged', () => {
     expect(formatHtml('plain text response')).toEqual('plain text response');
   });
+  it('should strip tags from html', () => {
+    expect(formatHtml('<html><body>Hello world</body></html>')).not.toContain('<html><body>');
+  });
   it('should strip css from html', () => {
     const mockResponse = '<!doctype html><html><head><style type="text/css">body {background-color: #f0f0f2;}</style></head></html>';
-    expect(formatHtml(mockResponse)).not.toContain('<style');
+    expect(formatHtml(mockResponse)).not.toContain('body {background-color: #f0f0f2;}');
   });
 });
