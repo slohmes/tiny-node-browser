426e91c46a0f6b9d43165feb563e387fac1f25ad
Sarah Lohmeier <sarahlohmes@gmail.com>
2018-05-31 16:54:52 -0400

    Permit character encodings in content-type html response headers

--- spec/getHtml-spec.js
+++ spec/getHtml-spec.js
@@ -24,7 +24,7 @@ describe('getHtml', () => {
     expect(https.get).toHaveBeenCalled();
   });
 
-  it('should throw an error when status code of http response is not 2xx', () => {
+  it('should throw an error when status code of http response is not 2xx', (done) => {
     spyOn(http, 'get').and.callFake((url, callback) => (callback({
         statusCode: 406,
         statusMessage: 'Not Acceptable',
@@ -33,10 +33,11 @@ describe('getHtml', () => {
     ));
     getHtml(exampleDotCom).catch((err) => {
        expect(err.name).toContain('406');
+       done();
     });
   });
 
-  it('should throw error if response is not html or plain text', () => {
+  it('should throw error if response is not html or plain text', (done) => {
     spyOn(http, 'get').and.callFake((url, cb) => (cb({
       statusCode: 200,
       headers: {'content-type': 'audio/ogg'},
@@ -45,10 +46,11 @@ describe('getHtml', () => {
     })));
     getHtml(exampleDotCom).catch((err) => {
       expect(err).toBeDefined();
+      done();
     });
   });
 
-  it('should return text if response is plain text', () => {
+  it('should return text if response is plain text', (done) => {
     const hostname = '127.0.0.1';
     const port = 3000;
 
@@ -56,13 +58,14 @@ describe('getHtml', () => {
       res.writeHead(200, {'content-type': 'text/plain'});
       res.end('la de dah');
     }).listen(port, hostname);
- 
+
     getHtml(`http://${hostname}:${port}/`).then(res => {
       expect(res).toEqual('la de dah');
+      done();
     })
   });
 
-  it('should return html when valid http response is returned', () => {
+  it('should return html when valid http response is returned', (done) => {
     const hostname = '127.0.0.1';
     const port = 3001;
 
@@ -73,6 +76,22 @@ describe('getHtml', () => {
 
     getHtml(`http://${hostname}:${port}/`).then(res => {
       expect(res).toContain('<html>');
+      done();
+    });
+  });
+
+  it('should return html when http contentType header contains character encoding', (done) => {
+    const hostname = '127.0.0.1';
+    const port = 3002;
+
+    const testServer = http.createServer((req, res) => {
+      res.writeHead(200, {'content-type': 'text/html; charset=UTF-8'});
+      res.end('<html>hello world</html>');
+    }).listen(port, hostname);
+
+    getHtml(`http://${hostname}:${port}/`).then(res => {
+      expect(res).toContain('<html>');
+      done();
     });
   });
 });
